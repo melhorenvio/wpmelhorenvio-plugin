5b96800552bf33ed72e51515a2780fb91117b9d4
matheus thomaz <matheus@melhorenvio.com.br>
2018-01-26 17:46:40 -0200

    ajustes

--- views/apikey.php
+++ views/apikey.php
@@ -36,8 +36,7 @@
             <h1>Melhor Envio</h1>
         </div>
         <form action="<?= $_SERVER['REQUEST_URI'] ?>" method="post">
-
-            <label>Cole aqui seu Token de acesso</label>
+            <h2>Cole aqui seu Token de acesso</h2>
             <?php wp_nonce_field('wpmelhorenvio_apikey_nonce'); ?>
             <textarea type="text" class="wpme_inputtext" name="wpmelhorenvio_token" ><?=get_option('wpmelhorenvio_token')?></textarea> <br>
             <p>Para utilizar o Plugin é necessário estar cadastrado no <a href="https://melhorenvio.com.br">Melhor Envio</a>.</p>
